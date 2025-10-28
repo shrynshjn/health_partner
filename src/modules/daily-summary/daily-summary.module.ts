8dfc240e319740f5c12282b5d3992c0afa1935e3
Shreyansh Jain <shreyanshmacro@gmail.com>
2025-10-28 22:47:05 +0530

    fixed imports

--- src/modules/daily-summary/daily-summary.module.ts
+++ src/modules/daily-summary/daily-summary.module.ts
@@ -3,11 +3,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DailySummaryController } from './daily-summary.controller';
 import { DailySummaryService } from './daily-summary.service';
 import { DailySummary, DailySummarySchema } from './daily-summary.schema';
+import { Food, FoodSchema } from '../food/food.schema';
+import { Workout, WorkoutSchema } from '../workout/workout.schema';
+import { Water, WaterSchema } from '../water/water.schema';
+import { Sleep, SleepSchema } from '../sleep/sleep.schema';
+import { Goals, GoalsSchema } from '../goals/goals.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: DailySummary.name, schema: DailySummarySchema },
+      {name: Food.name, schema: FoodSchema},
+      {name: Workout.name, schema: WorkoutSchema},
+      {name: Water.name, schema: WaterSchema},
+      {name: Sleep.name, schema: SleepSchema},
+      {name: Goals.name, schema: GoalsSchema},
     ]),
   ],
   controllers: [DailySummaryController],
